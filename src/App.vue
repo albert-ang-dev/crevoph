93b8b442624527d6db91a563245e0ca4d37f2781
albert-ang-dev <anga44574@gmail.com>
2026-06-16 12:53:27 -0400

    initial-submit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center mt-5">Weldcome to Crevo PH</h1>
+    <p class="text-center">Your one-stop solution for all your creative needs.</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
