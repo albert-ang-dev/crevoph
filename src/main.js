7cba3efb700ef6ebec2862a8cfe5fd7d96fee2b8
albert-ang-dev <anga44574@gmail.com>
2026-06-21 20:08:33 -0400

    start-fresh

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css' 
-import App from './App.vue';
-
-createApp(App).mount('#app')
